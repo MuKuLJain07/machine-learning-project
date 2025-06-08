d8f2563f25e04b9efbffed3034e5df0c1ed5adc0
Mukul Jain <81482724+MuKuLJain07@users.noreply.github.com>
2025-06-09 00:38:07 +0530

    added jupyter notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     error = str(error)
-    error_message="Error occured in the python script name [{0}] at line number [{1}]. \n Error message : [{3}]".format(file_name, line_number, error)
+    error_message="Error occured in the python script name [{0}] at line number [{1}]. \n Error message : [{2}]".format(file_name, line_number, error)
 
     return error_message
 
@@ -17,3 +18,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e, sys)
